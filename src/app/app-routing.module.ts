a7358fba6bb92c57665b1269bd9c48fc60971efa
mrcthle <mirco.seghorn@gmail.com>
2023-01-25 11:11:17 +0100

    Adds route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import{ EmployeeListComponent } from "./employee-list/employee-list.component";
 import {AuthGuard} from "./utitlity/app.guard";
 import {QualificationListComponent} from "./qualification-list/qualification-list.component";
+import {EmployeeDetailComponent} from "./employee-detail/employee-detail.component";
 
 const routes: Routes = [
   { path: '', redirectTo: '/employee', pathMatch: 'full' },
   { path: 'employee', component: EmployeeListComponent, canActivate: [AuthGuard]},
   { path: 'qualifications', component: QualificationListComponent, canActivate: [AuthGuard]},
+  { path: 'employee/:id', component: EmployeeDetailComponent, canActivate: [AuthGuard]},
 ];
 
 @NgModule({
