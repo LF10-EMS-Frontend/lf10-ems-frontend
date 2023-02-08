b4a058fa5a662aa07f38fe52c795cc8cdd6fe9d0
mrcthle <mirco.seghorn@gmail.com>
2023-02-08 11:05:44 +0100

    Refactors component

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,6 +1,6 @@
 import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {KeycloakService} from "keycloak-angular";
-import {debounceTime, distinctUntilChanged, startWith, tap} from "rxjs";
+import {debounceTime, distinctUntilChanged, startWith} from "rxjs";
 import {FormControl} from "@angular/forms";
 import {NgbModal, NgbModalOptions} from "@ng-bootstrap/ng-bootstrap";
 import {PopupComponent} from "../popup/popup.component";
@@ -16,8 +16,8 @@ export class NavbarComponent implements OnInit {
   @Input() popups:boolean = false;
   @Input() yesNo:boolean = false;
   @Input() searchbar:boolean = true;
-  @Input() header:string = 'Warning';
-  @Input() content:string = 'Do you really want to continue?';
+  @Input() header:string = 'Leave confirmation';
+  @Input() content:string = 'Do you really want to leave the page?';
 
   constructor(private keycloakService: KeycloakService, private modalService: NgbModal, private router: Router,) {
   }
@@ -34,26 +34,35 @@ export class NavbarComponent implements OnInit {
   }
 
   logout(): void {
-    this.keycloakService.logout('http://localhost:4200/').then();
+    this.yesNo = true;
+    this.openPopup('Logout confirmation', 'Do you really want to logout?').then((r) => {
+      if (r) {
+        this.keycloakService.logout('http://localhost:4200/').then();
+      }
+    })
   }
 
-  openPopup(destination:string) {
+  openCancelPopup(destination:string) {
     if (!this.popups) {
       this.router.navigate([destination]).then();
     } else {
-      let ngbModalOptions: NgbModalOptions = {
-        backdrop : 'static',
-        keyboard : false
-      };
-      const modalRef = this.modalService.open(PopupComponent, ngbModalOptions);
-      modalRef.componentInstance.header = this.header;
-      modalRef.componentInstance.content = this.content;
-      modalRef.componentInstance.decision = this.yesNo;
-      modalRef.result.then((r) => {
+      this.openPopup(this.header, this.content).then((r) => {
         if (r == true) {
           this.router.navigate([destination]).then();
         }
       });
     }
+  }
+
+  private openPopup(header:string, content:string) {
+    let ngbModalOptions: NgbModalOptions = {
+      backdrop : 'static',
+      keyboard : false
+    };
+    const modalRef = this.modalService.open(PopupComponent, ngbModalOptions);
+    modalRef.componentInstance.header = header;
+    modalRef.componentInstance.content = content;
+    modalRef.componentInstance.decision = this.yesNo;
+    return modalRef.result;
   }
 }
